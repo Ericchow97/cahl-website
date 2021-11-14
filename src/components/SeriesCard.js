dd35da497e60562d32f9bc1f0ecb6e6ff8858bcd
Eric Chow <eric.chow803@gmail.com>
2021-11-14 10:23:21 -0500

    Update SeriesCard.js
    update max 7 games in series

--- src/components/SeriesCard.js
+++ src/components/SeriesCard.js
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router';
 import { Row, Col, Divider } from 'antd';
 
 export const SeriesCard = (props) => {
-    const [ redirect, setRedirect ] = useState(false)
+    const [redirect, setRedirect] = useState(false)
 
     const handleClick = () => {
         setRedirect(true)
@@ -39,56 +39,56 @@ export const SeriesCard = (props) => {
             team2Jersey: 'Dark',
         },
     ]
-    
+
     let gameNumber = 0
     if (props.seriesGames && props.seriesGames.length) {
-       gameNumber = props.seriesGames.length
+        gameNumber = Math.min(props.seriesGames.length, 6)
     }
 
     return (
         <>
-            {redirect && <Redirect push to = {`/teams/${props.seriesId}`}/>}
+            {redirect && <Redirect push to={`/teams/${props.seriesId}`} />}
             {props.divider && <Divider />}
-            <div onClick={props.disableRedirect ? undefined : handleClick} style={{cursor: !props.disableRedirect && 'pointer'}}>
-                <Row gutter={24} style={{display: 'inherit'}}>
+            <div onClick={props.disableRedirect ? undefined : handleClick} style={{ cursor: !props.disableRedirect && 'pointer' }}>
+                <Row gutter={24} style={{ display: 'inherit' }}>
                     <Col >
                         <h2>{props.title}</h2>
                     </Col>
                 </Row>
-                <Row gutter={24}> 
+                <Row gutter={24}>
                     <Col span={10}>
                         <h2>{props.team1Name}</h2>
                     </Col>
                     <Col span={4}>
-                        <h3 style={{margin: "7px"}}>VS</h3>
+                        <h3 style={{ margin: "7px" }}>VS</h3>
                     </Col>
                     <Col span={10}>
                         <h2>{props.team2Name}</h2>
                     </Col>
                 </Row>
                 {props.seriesGames &&
-                <Row gutter={24}>
-                    <Col span={10}>
-                        <p>Jersey Colour: {jerseySet[gameNumber].team1Jersey}</p>
-                    </Col>
-                    <Col span={4}>
-                    </Col>
-                    <Col span={10}>
-                        <p>Jersey Colour: {jerseySet[gameNumber].team2Jersey}</p>
-                    </Col>
-                </Row>
+                    <Row gutter={24}>
+                        <Col span={10}>
+                            <p>Jersey Colour: {jerseySet[gameNumber].team1Jersey}</p>
+                        </Col>
+                        <Col span={4}>
+                        </Col>
+                        <Col span={10}>
+                            <p>Jersey Colour: {jerseySet[gameNumber].team2Jersey}</p>
+                        </Col>
+                    </Row>
                 }
                 {props.captain &&
-                <Row gutter={24}>
-                    <Col span={10}>
-                        <p>Captain: {props.captain1}</p>
-                    </Col>
-                    <Col span={4}>
-                    </Col>
-                    <Col span={10}>
-                        <p>Captain: {props.captain2}</p>
-                    </Col>
-                </Row>
+                    <Row gutter={24}>
+                        <Col span={10}>
+                            <p>Captain: {props.captain1}</p>
+                        </Col>
+                        <Col span={4}>
+                        </Col>
+                        <Col span={10}>
+                            <p>Captain: {props.captain2}</p>
+                        </Col>
+                    </Row>
                 }
                 <Row gutter={24}>
                     <Col span={10}>
@@ -102,8 +102,8 @@ export const SeriesCard = (props) => {
                     </Col>
                 </Row>
             </div>
-            {props.SeriesCarousel && <props.SeriesCarousel seriesGames={props.seriesGames} team1Name={props.team1Name} team2Name={props.team2Name}/> }
-            
+            {props.SeriesCarousel && <props.SeriesCarousel seriesGames={props.seriesGames} team1Name={props.team1Name} team2Name={props.team2Name} />}
+
         </>
     )
 }
